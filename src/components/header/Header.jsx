eedd6fee2232f65868987b94d5127689c3fc3d2f
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-02-07 11:26:17 -0300

    create Header component and add images

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='w-full bg-black h-20 px-2 '>
+            <div>
+                <img src="" alt="" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
